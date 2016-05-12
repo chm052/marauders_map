01ab161ee16d629684c6fc2495170754696ac742
jespinas <janella.espinas@gmail.com>
2016-05-12 12:12:35 +1200

    Add dummy Truck data for /trucks endpoint

--- server.go
+++ server.go
@@ -25,7 +25,12 @@ func SayHelloWorld(w http.ResponseWriter, r *http.Request) {
 
 func GetFoodTrucks(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json; charset=utf=8")
-  myItems := []string{"item1", "item2", "item3"}
+
+  truck1 := Truck{1, "Greek Food Truck", "-41.292489", "174.778656"}
+  truck2 := Truck{2, "Beat Kitchen", "-41.287022", "174.778667"}
+  truck3 := Truck{3, "Nanny's Food Truck", "-41.290425", "174.779272"}
+  myItems := []Truck{truck1, truck2, truck3}
+
   a, _:= json.Marshal(myItems)
   w.Write(a)
   return
---
+++ truck.go
@@ -0,0 +1,8 @@
+package main
+
+type Truck struct {
+  FoodTruckId int
+  Name string
+  Latitude string
+  Longitude string
+}
