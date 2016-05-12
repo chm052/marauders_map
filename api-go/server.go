a23c3ed74c42b473c5d48f3356d30f3788f9d446
Caitlin Miller <caitlin.miller@xero.com>
2016-05-12 15:20:15 +1200

    Moved list of trucks to global variable.

--- api-go/server.go
+++ api-go/server.go
@@ -7,7 +7,15 @@ import (
   "encoding/json"
 )
 
+var truck1 = Truck{1, "Greek Food Truck", "-41.292489", "174.778656"}
+var truck2 = Truck{2, "Beat Kitchen", "-41.287022", "174.778667"}
+var truck3 = Truck{3, "Nanny's Food Truck", "-41.290425", "174.779272"}
+var allTrucks = []Truck{truck1, truck2, truck3}
+
 func main() {
+
+
+
   mx := mux.NewRouter()
 
   mx.HandleFunc("/", SayHelloWorld)
@@ -62,13 +70,7 @@ func PostFoodTruckLocation(w http.ResponseWriter, r *http.Request) {
 
 func GetFoodTrucks(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json; charset=utf=8")
-
-  truck1 := Truck{1, "Greek Food Truck", "-41.292489", "174.778656"}
-  truck2 := Truck{2, "Beat Kitchen", "-41.287022", "174.778667"}
-  truck3 := Truck{3, "Nanny's Food Truck", "-41.290425", "174.779272"}
-  myItems := []Truck{truck1, truck2, truck3}
-
-  a, _:= json.Marshal(myItems)
+  a, _:= json.Marshal(allTrucks)
   w.Write(a)
   return
 }
