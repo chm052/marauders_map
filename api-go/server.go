dc32d1dce469d88148caf01a1c9cea9ec2b9a235
John Clegg <john@projectx.co.nz>
2016-05-12 21:33:45 +1200

    Draft foodtrucks nearby
    Add the query to search for food trucks within 1000m of lat long location provided

--- api-go/server.go
+++ api-go/server.go
@@ -30,6 +30,22 @@ func main() {
   http.ListenAndServe(":9001", mx)
 }
 
+func GetNearbyFoodTrucks(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json; charset=utf=8")
+  latitude, err2 := strconv.ParseFloat(queryParameters.Get("lat"), 64)
+  longitude, err3 := strconv.ParseFloat(queryParameters.Get("lon"), 64)
+  trucks := []Truck{}
+  err := db.Queryx("SELECT id, name, owner_id, lat, lng, url FROM FoodTrucks WHERE ST_DWithin(geom,  ST_GeomFromText('POINT($1 $2)', 4326),1000,false)", longitude,latitude).StructScan(&truck)
+  fmt.Println(trucks)
+  if (err != nil){
+    fmt.Println(err)
+    return
+  }
+  a, _:= json.Marshal(trucks)
+  w.Write(a)
+  return
+}
+
 func GetFoodTrucks(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json; charset=utf=8")
   trucks := []Truck{}
